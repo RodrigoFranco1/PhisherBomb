89e590cb1235d95bb880950c4b3bd9d6e92273c8
RodrigoFranco1 <115948997+RodrigoFranco1@users.noreply.github.com>
2023-12-06 09:29:06 -0600

    Update PhisherBomb.py

--- PhisherBomb.py
+++ PhisherBomb.py
@@ -1,5 +1,6 @@
 import requests
 from faker import Faker
+import argparse
 
 def generar_usuario_falso():
     fake = Faker()
@@ -11,24 +12,23 @@ def generar_usuario_falso():
 
 def enviar_info(url, usuario):
     try:
-        # Realizar la solicitud POST
         response = requests.post(url, data=usuario)
-
-        # Verificar si la solicitud fue exitosa
         if response.status_code == 200:
             print("Datos enviados correctamente.")
         else:
-            print(f"Ocurrió un error al enviar los datos. Código de estado: {response.status_code}")
-
+            print(f"Error al enviar datos. Código de estado: {response.status_code}")
     except requests.RequestException as e:
-        print(f"Ocurrió un error al enviar los datos: {e}")
+        print(f"Error al enviar datos: {e}")
 
-# URL del sitio de phishing
-url_phishing = 'http://unbouncepages.com/aadsafgsdgvxbv/'
+def main(url, numero_intentos):
+    for _ in range(numero_intentos):
+        usuario_falso = generar_usuario_falso()
+        enviar_info(url, usuario_falso)
 
-# Número de intentos que quieres realizar
-numero_intentos = 1000
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="PhisherBomb: Herramienta para inundar sitios de phishing con datos falsos.")
+    parser.add_argument("-u", "--url", required=True, help="URL del sitio de phishing")
+    parser.add_argument("-r", "--requests", type=int, default=100, help="Número de solicitudes falsas a enviar")
+    args = parser.parse_args()
 
-for _ in range(numero_intentos):
-    usuario_falso = generar_usuario_falso()
-    enviar_info(url_phishing, usuario_falso)
+    main(args.url, args.requests)
